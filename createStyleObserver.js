94ea880dddf27f587c6e207cfdeebb5137297096
Chris Myers <christopherbm@gmail.com>
2016-05-24 08:36:50 -0700

    create style observer dups

--- createStyleObserver.js
+++ createStyleObserver.js
@@ -1,7 +1,14 @@
 module.exports = function() {
+  const createMutationObserver = require('./createMutationObserver')();
+  const createAttrObserverConfig = require('./createConfigs/createAttrObserverConfig')();
+  const styleObserverHandler = require('./styleObserverHandler')();
+  const setDataAttr = require('myrs-js-core/fe/setDataAttr')();
+  const capitalize = require('myrs-js-core/shared/capitalize')();
+  const getStyle = require('myrs-js-core/fe/getStyle')();
+
   return function createStyleObserver(sel, style, fn) {
     var el = sel();
-    var mo = createMutationObserver(el, createAttrObserverConfig(), styleObserverHandler(sel));
+    var mo = createMutationObserver(el, createAttrObserverConfig(), styleObserverHandler(sel, fn));
     setDataAttr(el, 'obs' + capitalize(style), getStyle(el, style));
     el = null;
     return mo;
