0749f95a1c4eb74473a07134956d9bf69f4d775b
Chris Myers <christopherbm@gmail.com>
2016-05-24 08:34:01 -0700

    create mutation observer dups

--- createMutationObserver.js
+++ createMutationObserver.js
@@ -1,5 +1,6 @@
 module.exports = function() {
-  // !!!
+  const setDataAttr = require('myrs-js-core/fe/setDataAttr')();
+
   return function createMutationObserver(el, config, fn) {
     setDataAttr(el, 'observed', true);
     var mo = new MutationObserver(fn); 
