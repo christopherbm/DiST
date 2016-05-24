3ee62277495db5ec001908dcc6a6deb75afe4a7f
Chris Myers <christopherbm@gmail.com>
2016-05-24 12:25:33 -0700

    request observer

---
+++ createRequestObserver.js
@@ -0,0 +1,23 @@
+module.exports = function() {
+  const createMutationObserver = require('./createMutationObserver')();
+  const createAttrObserverConfig = require('./createConfigs/createAttrObserverConfig')();
+  const styleObserverHandler = require('./styleObserverHandler')();
+  const setDataAttr = require('myrs-js-core/fe/setDataAttr')();
+  const capitalize = require('myrs-js-core/shared/capitalize')();
+  const getStyle = require('myrs-js-core/fe/getStyle')();
+
+  return function createRequestObserver(sel, fn) {
+    var style = 'display';
+    var el = sel();
+    var mo = createMutationObserver(
+      el, 
+      createAttrObserverConfig(), 
+      styleObserverHandler(sel, style, () => {
+        console.log('style observed');
+        fn({});
+      })));
+    setDataAttr(el, 'obs' + capitalize(style), getStyle(el, style));
+    el = null;
+    return mo;
+  }
+};
