0a1c885fecbf3334fe49403ea4577fea3a69ad10
Chris Myers <christopherbm@gmail.com>
2016-05-26 10:40:06 -0700

    fix dups

--- getStateByEvent.js
+++ getStateByEvent.js
@@ -1,4 +1,6 @@
 module.exports = function() {
+  const createStateEvent = require('./createStateEvent')();
+
   return function getStateByEvent(sel, fn) {
     sel().dispatchEvent(createStateEvent(function(ev, state) { fn(state); }));
   }
