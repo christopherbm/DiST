16d97a975759dd5092b5f2408b5d3b91f3c05762
Chris Myers <christopherbm@gmail.com>
2016-06-02 17:40:04 -0700

    temp add to all

--- onChildChange.js
+++ onChildChange.js
@@ -3,10 +3,11 @@ module.exports = function() {
 
   // make sure this is clean that it only works on direct children (not recursive)
   return function onChildChange(sel, addedFn, removedFn) {
-    var args = Array.prototype.slice.call(arguments);
-    if (args.length === 1) {
-      createOnChildChangeObserver(sel, addedFn, removedFn);
-    }
+    //var args = Array.prototype.slice.call(arguments);
+    //if (args.length === 1) {
+    //  createOnChildChangeObserver(sel, addedFn, removedFn);
+    //}
+    createOnChildChangeObserver(sel, addedFn, removedFn);
     //sel().addEventListener('child.change', fn, false);
   }
 };
